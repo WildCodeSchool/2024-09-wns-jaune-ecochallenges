8c6be732c0e35656c398c19c5898345f3ac5e0d1
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2025-03-08 20:22:26 +0100

    install lint staged husky
    install lint staged husky
    
    install lint staged husky
    
    install lint staged husky
    
    install lint staged husky
    
    install lint staged husky
    
    hook functionnal
    
    remove eslint from frontend as it is in root
    
    remove test code from app.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -22,12 +22,12 @@ const typeDefs = `#graphql
 `;
 const books = [
   {
-    title: "The Awakening",
-    author: "Kate Chopin",
+    title: 'The Awakening',
+    author: 'Kate Chopin',
   },
   {
-    title: "City of Glass",
-    author: "Paul Auster",
+    title: 'City of Glass',
+    author: 'Paul Auster',
   },
 ];
 // Resolvers define how to fetch the types defined in your schema.
