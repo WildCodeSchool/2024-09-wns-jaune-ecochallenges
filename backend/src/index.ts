cb35e7ab3d9331f6c026f364d318c0b275cf754f
marjorie <marjorie@homeexchange.com>
2025-03-11 17:06:56 +0100

    feat: add backend_port to nginx.dev.conf

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,8 +4,11 @@ import { dataSource } from './config/db';
 import { UserResolver } from './resolvers/UserResolver';
 import { buildSchema } from 'type-graphql';
 import 'reflect-metadata';
+import { config } from "dotenv";
 
-const port = process.env.BACK_PORT ? parseInt(process.env.BACK_PORT) : 4005;
+config();
+const port = Number(process.env.BACKEND_PORT);
+if (!port) throw new Error("Missing env variable: BACKEND_PORT");
 
 async function start() {
   await dataSource.initialize();
@@ -17,7 +20,7 @@ async function start() {
   const server = new ApolloServer({ schema });
 
   const { url } = await startStandaloneServer(server, {
-    listen: { port: port },
+    listen: { port: port, host: '0.0.0.0' },
   });
 
   console.log(`🚀  Server ready at: ${url}`);
