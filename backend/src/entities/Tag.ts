e7828322380a754d3aa4c3ec12844b0b14bd59d4
Elie Bendier <elie.bendier.dev@gmail.com>
2025-03-13 09:22:59 +0100

    fix tag entity by adding/correcting decorator

--- backend/src/entities/Tag.ts
+++ backend/src/entities/Tag.ts
@@ -2,24 +2,28 @@ import {
   BaseEntity,
   Column,
   Entity,
+  Index,
   ManyToMany,
   PrimaryGeneratedColumn,
+  Unique,
 } from 'typeorm';
 import { Field, ID, ObjectType } from 'type-graphql';
 import { Action } from './Action';
 
 @Entity()
 @ObjectType()
+@Unique(['label'])
 export class Tag extends BaseEntity {
   @PrimaryGeneratedColumn()
   @Field((_type) => ID)
   id!: number;
 
   @Field()
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: false })
+  @Index()
   label!: string;
 
-  @Field((_type) => [Action], { nullable: false })
+  @Field((_type) => [Action], { nullable: true })
   @ManyToMany((_type) => Action, (action) => action.tags)
-  actions!: Action[];
+  actions?: Action[];
 }
