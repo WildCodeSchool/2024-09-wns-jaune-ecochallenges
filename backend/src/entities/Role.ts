b82750df20732367a49812bf4936a334a0da42db
Elie Bendier <elie.bendier.dev@gmail.com>
2025-03-13 09:26:30 +0100

    fix role entity by adding/correcting decorator

--- backend/src/entities/Role.ts
+++ backend/src/entities/Role.ts
@@ -2,24 +2,28 @@ import {
   BaseEntity,
   Column,
   Entity,
+  Index,
   OneToMany,
   PrimaryGeneratedColumn,
+  Unique,
 } from 'typeorm';
 import { Field, ID, ObjectType } from 'type-graphql';
 import { User } from './User';
 
 @Entity()
 @ObjectType()
+@Unique(['label'])
 export class Role extends BaseEntity {
   @PrimaryGeneratedColumn()
   @Field((_type) => ID)
   id!: number;
 
   @Field()
-  @Column({ length: 100 })
+  @Column({ length: 100, nullable: false })
+  @Index()
   label!: string;
 
-  @Field((_type) => User)
+  @Field((_type) => [User], { nullable: true })
   @OneToMany(() => User, (user) => user.role)
-  users!: User[];
+  users?: User[];
 }
