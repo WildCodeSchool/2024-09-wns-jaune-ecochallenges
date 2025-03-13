caf309401784e002ca68b326369dcde91ee617ed
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2025-03-13 17:00:05 +0100

    add a service to display static images from persist data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,8 @@ function App() {
       <h1 className="rounded-full text-3xl font-bold text-red-500 underline">
         Hello world with tailwind css!
       </h1>
+      <img src="icons/leaf.png" alt="test" />
+      <img src="/files/icons/water.png" alt="test" />
       <Button>Click me with shadcn/ui</Button>
       <Outlet />
     </>
