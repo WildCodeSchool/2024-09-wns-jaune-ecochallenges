40157555859aa6f78a4e22992bd2c3ba48d915a6
Elie Bendier <elie.bendier.dev@gmail.com>
2025-03-13 12:25:59 +0100

    create challengeResolver and getChallenges query

---
+++ backend/src/resolvers/ChallengeResolver.ts
@@ -0,0 +1,11 @@
+import { Query, Resolver } from 'type-graphql';
+import { Challenge } from '../entities/Challenge';
+
+@Resolver(Challenge)
+class ChallengeResolver {
+  @Query(() => [Challenge])
+  async getChallenges(): Promise<Challenge[]> {
+    const challenges = await Challenge.find();
+    return challenges;
+  }
+}
