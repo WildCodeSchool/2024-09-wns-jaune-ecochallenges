4d1c7a9d023eac0f5901eeac80ce1397ebfd2818
marjorie <marjorie@homeexchange.com>
2025-03-16 15:12:27 +0100

    add mobile first to action card

--- frontend/src/components/ActionCard.tsx
+++ frontend/src/components/ActionCard.tsx
@@ -6,8 +6,9 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import { Button } from '@/components/ui';
-import { Hourglass, ImagePlus, Plus } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { CirclePlus, Ellipsis, Hourglass, ImagePlus } from 'lucide-react';
+import { Pill } from './Pill';
 
 type ActionCardProps = {
   readonly action: Action;
@@ -17,42 +18,70 @@ export const ActionCard = ({ action }: ActionCardProps) => {
   return (
     <Card
       key={action.id}
-      className="m-2 flex h-40 justify-center bg-teal-50 p-2 xl:w-1/5"
+      className="3xl:w-[20vw] flex h-[15vh] w-[96vw] flex-col justify-center bg-teal-50 p-2 lg:flex-row xl:h-[12vh] xl:w-[30vw] 2xl:w-[25vw]"
     >
-      <div className="flex">
+      {/* // 1er block */}
+      <div className="basis-1/6 xl:h-full">
         <img
           src={`./icons/${action.icon}.png`}
           alt="name"
-          className="w-1/7 object-contain"
+          className="hidden object-contain p-2 lg:inline lg:h-full lg:w-full"
         />
-        <div className="flex w-full flex-col">
-          <CardHeader className="mb-3 flex flex-col text-left">
-            <CardTitle className="w-full">{action.name}</CardTitle>
-            <CardDescription className="w-full">
-              {action.description}
-            </CardDescription>
-          </CardHeader>
-          <CardFooter className="flex w-full justify-around">
-            <Button type="button" variant="ghost" className="cursor-pointer">
+      </div>
+      {/* // 2ème block */}
+      <div className="flex basis-4/6 flex-col content-evenly gap-4 self-center">
+        <CardHeader className="flex flex-col text-left">
+          <CardTitle className="text-lg font-bold">{action.name}</CardTitle>
+          <CardDescription className="text-base font-bold">
+            {action.description}
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className="flex justify-around">
+          <Pill className="p-3">
+            <Button
+              type="button"
+              variant="ghost"
+              className="cursor-pointer hover:bg-transparent hover:opacity-100"
+            >
               <ImagePlus
-                className=""
-                style={{ minWidth: '30px', minHeight: '30px' }}
+                strokeWidth={3}
+                style={{ minWidth: '25px', minHeight: '25px' }}
               />
             </Button>
+          </Pill>
+          <Pill className="p-3">
             <img
-              className="w-10"
+              className="w-7"
               src={`./icons/level-${action.level}.png`}
               alt="icon of dificulty level"
             />
-
-            <div className="flex items-center justify-center">
-              <p className="text-2xl">{action.time}h</p>
-              <Hourglass className="" size={25} />
-            </div>
-          </CardFooter>
-        </div>
-        <Button type="button" variant="ghost" className="m-auto cursor-pointer">
-          <Plus style={{ minWidth: '36px', minHeight: '36px' }} />
+          </Pill>
+          <Pill className="p-3">
+            <Hourglass strokeWidth={3} />
+            <span className="text-base font-bold">{action.time}h</span>
+          </Pill>
+          <Pill className="p-3">
+            <Button
+              type="button"
+              variant="ghost"
+              className="cursor-pointer hover:bg-transparent hover:opacity-100"
+            >
+              <Ellipsis strokeWidth={3} />
+            </Button>
+          </Pill>
+        </CardFooter>
+      </div>
+      {/* // 3ème block */}
+      <div className="flex basis-1/6 self-center">
+        <Button
+          type="button"
+          variant="ghost"
+          className="cursor-pointer hover:bg-transparent hover:opacity-100"
+        >
+          <CirclePlus
+            style={{ minWidth: '45px', minHeight: '45px' }}
+            strokeWidth={3}
+          />
         </Button>
       </div>
     </Card>
