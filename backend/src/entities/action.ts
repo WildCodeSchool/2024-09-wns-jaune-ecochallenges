37d3c86677ab8aa96e17ef361f2498c9a4b255ea
Elie Bendier <elie.bendier.dev@gmail.com>
2025-04-07 16:50:03 +0200

    Action challenges ! => ?

--- backend/src/entities/action.ts
+++ backend/src/entities/action.ts
@@ -59,5 +59,5 @@ export class Action extends BaseEntity {
 
   @Field(() => [Challenge])
   @ManyToMany(() => Challenge, (challenge) => challenge.actions)
-  challenges!: Challenge[];
+  challenges?: Challenge[];
 }
