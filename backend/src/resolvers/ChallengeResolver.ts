7dfee604cc6b913ada21f4975aafc693f3012cd6
Elie Bendier <elie.bendier.dev@gmail.com>
2025-04-07 16:30:30 +0200

    add createChallenge mutation and ChallengeInput to ChallengeResolver

--- backend/src/resolvers/ChallengeResolver.ts
+++ backend/src/resolvers/ChallengeResolver.ts
@@ -1,5 +1,23 @@
-import { Query, Resolver } from 'type-graphql';
+import { Arg, Field, InputType, Mutation, Query, Resolver } from 'type-graphql';
 import { Challenge } from '@/entities';
+
+@InputType()
+class ChallengeInput {
+  @Field()
+  label!: string;
+
+  @Field()
+  description?: string;
+
+  @Field()
+  bannerUrl?: string;
+
+  @Field()
+  startDate!: Date;
+
+  @Field()
+  endDate!: Date;
+}
 
 @Resolver(Challenge)
 export class ChallengeResolver {
@@ -7,5 +25,18 @@ export class ChallengeResolver {
   async getChallenges(): Promise<Challenge[]> {
     const challenges = await Challenge.find();
     return challenges;
+  }
+
+  @Mutation(() => Challenge)
+  async createChallenge(@Arg('data') data: ChallengeInput) {
+    try {
+      let challenge = new Challenge();
+      challenge = Object.assign(challenge, data);
+      await challenge.save();
+
+      return challenge;
+    } catch (err) {
+      throw new Error(`Failed to create challenge: ${err}`);
+    }
   }
 }
