4ac57a2d108a48f9aa2e1517b73b5cafd86a56bc
Ornella <borges.ornella@gmail.com>
2025-04-08 15:05:13 +0200

    feat:Add stats icon to navbar and highlight active navigation item based on current route using useLocation from RRDom.

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,24 +1,36 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Button } from '@/components/ui';
+import { Goal, HandCoins, House, UserRound, BarChart3 } from 'lucide-react';
+
+const navItems = [
+  { to: '/challenges', icon: Goal, label: 'Challenges' },
+  { to: '/actions', icon: HandCoins, label: 'Actions' },
+  { to: '/home', icon: House, label: 'Home' },
+  { to: '/stats', icon: BarChart3, label: 'Stats' },
+  { to: '/user', icon: UserRound, label: 'User' },
+];
 
 export const Navbar = () => {
+  const location = useLocation();
+
   return (
-    <nav className="h-navbar fixed bottom-0 z-50 flex w-full items-center justify-around bg-gray-800 p-4 text-white">
-      <Button variant="ghost">
-        <Link to="/home" className="text-white hover:text-gray-400">
-          Home
-        </Link>
-      </Button>
-      <Button variant="ghost">
-        <Link to="/challenges" className="text-white hover:text-gray-400">
-          Challenges
-        </Link>
-      </Button>
-      <Button variant="ghost">
-        <Link to="/actions" className="text-white hover:text-gray-400">
-          Actions
-        </Link>
-      </Button>
+    <nav className="h-navbar fixed bottom-0 z-50 flex w-full items-center justify-around bg-green-800 p-4 text-white">
+      {navItems.map(({ to, icon: Icon, label }) => {
+        const isActive = location.pathname === to;
+
+        return (
+          <Button variant="ghost" key={to} aria-label={label}>
+            <Link
+              to={to}
+              className={`flex flex-col items-center transition-colors ${
+                isActive ? 'text-blue-500' : 'text-white'
+              } hover:text-gray-400`}
+            >
+              <Icon />
+            </Link>
+          </Button>
+        );
+      })}
     </nav>
   );
 };
