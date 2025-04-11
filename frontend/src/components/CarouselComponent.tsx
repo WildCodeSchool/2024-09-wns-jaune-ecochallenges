648809058e56b39ac908ffebdc210be42295035a
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2025-04-11 12:12:34 +0200

    add carousel option infinite loop

--- frontend/src/components/CarouselComponent.tsx
+++ frontend/src/components/CarouselComponent.tsx
@@ -34,18 +34,15 @@ export const CarouselComponent = <T,>({
     api.on('select', () => {
       const newCurrent = api.selectedScrollSnap() + 1;
       setCurrent(newCurrent);
-
-      if (newCurrent === count) {
-        api.scrollTo(0);
-        setCurrent(1);
-        plugin.current.reset();
-      }
     });
   }, [api, count]);
 
   return (
     <div className="mx-auto max-w-xl">
       <Carousel
+        opts={{
+          loop: true,
+        }}
         setApi={setApi}
         plugins={[plugin.current]}
         onMouseEnter={plugin.current.stop}
@@ -64,9 +61,9 @@ export const CarouselComponent = <T,>({
         <CarouselNext className="hidden cursor-pointer sm:flex" />
       </Carousel>
       <div className="text-muted-foreground mt-2 flex flex-row justify-between pb-2 text-center align-baseline text-sm">
-        Challenge {current} sur {count - 1}
+        Challenge {current} sur {count}
         <div className="flex justify-center gap-2">
-          {scrollSnaps.slice(0, count - 1).map((_, index) => (
+          {scrollSnaps.slice(0, count).map((_, index) => (
             <div key={index} className="flex justify-center gap-2">
               <button
                 aria-label={`Aller au challenge ${index + 1}`}
