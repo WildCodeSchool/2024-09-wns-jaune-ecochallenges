40771bf1c1c4beb5432ff1b895ce479d8d6a4c0f
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2025-04-17 10:21:49 +0200

    add vizualizer in nginx

--- frontend/src/types/auth/login.types.ts
+++ frontend/src/types/auth/login.types.ts
@@ -1,4 +1,4 @@
 import { z } from 'zod';
-import { registerSchema } from '@/schemas/auth/login.schema';
+import { registerSchema } from '@/schemas/auth/register.schema';
 
 export type RegisterFormValues = z.infer<typeof registerSchema>;
