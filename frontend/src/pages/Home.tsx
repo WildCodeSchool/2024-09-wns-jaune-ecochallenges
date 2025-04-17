ea29d01b2d1bb1200697f6f1bee71e0915a2bd7b
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2025-04-17 08:17:50 +0200

    test on staging

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -63,6 +63,7 @@ export const Home = () => {
         data={ecoChallenges}
         CardComponent={ChallengeCarouselCard}
       />
+      <h1>THOMAS VOUS DIT BOJOUR DEPUIS LA STAGING BRANCH</h1>
     </>
   );
 };
