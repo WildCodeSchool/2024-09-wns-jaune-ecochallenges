f735ec90a38389b36cbe0c964cf438e1a7f834d2
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2025-04-18 22:02:06 +0200

    edit front page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -78,7 +78,7 @@ export const Home = () => {
         🌱 Bienvenue sur Eco-challenges 🌱
       </h1>
       <h1 className="mb-8 rounded-full text-center text-3xl font-thin">
-        Bonjour bonsoir
+        Bonjour
       </h1>
       {isAuth && <UserCard {...fakeDataUser}></UserCard>}
 
