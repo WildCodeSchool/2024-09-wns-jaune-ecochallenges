9d4ba2bd8643e73d030741869fd4e7cae59add10
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2025-04-18 21:54:36 +0200

    edit front page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -77,7 +77,9 @@ export const Home = () => {
       <h1 className="mb-8 rounded-full text-center text-3xl font-thin">
         🌱 Bienvenue sur Eco-challenges 🌱
       </h1>
-
+      <h1 className="mb-8 rounded-full text-center text-3xl font-thin">
+        Bonjour bonsoir
+      </h1>
       {isAuth && <UserCard {...fakeDataUser}></UserCard>}
 
       <CarouselComponent
