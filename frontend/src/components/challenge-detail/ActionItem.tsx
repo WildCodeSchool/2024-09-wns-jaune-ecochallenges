60d8ef104c13686a8a80e3195b6960f56b406287
Ornella B. <borges.ornella@gmail.com>
2025-06-12 14:21:50 +0200

    feat:Add custom Dialog component next to the H3 title in ActionItem.

--- frontend/src/components/challenge-detail/ActionItem.tsx
+++ frontend/src/components/challenge-detail/ActionItem.tsx
@@ -1,6 +1,7 @@
 import { Checkbox } from '@/components/ui/checkbox';
 import { Pill } from '@/components';
-import AvatarGroupDemo from './avatar-group'; // Assure-toi que ce chemin est correct
+import { CustomInfoDialog } from './CustomInfoDialog';
+import AvatarGroupDemo from './avatar-group';
 
 type Props = {
   id: string;
@@ -22,23 +23,22 @@ export const ActionItem = ({
   const isChecked = status === 'done';
 
   return (
-    <li className="flex items-center justify-between gap-2 rounded-xl bg-blue-300 p-4 shadow-sm">
-      <div className="flex items-center gap-4">
-        <div className="bg-zinc-200">
+    <li className="flex items-center justify-between gap-2 rounded-xl p-4 shadow-sm">
+      <div className="flex flex-col items-start gap-2">
+        <div className="flex items-center gap-2">
           <h3 className="block text-sm font-semibold">{title}</h3>
-          {/* <p className="text-muted-foreground flex items-center gap-2 text-xs">
-            {description}
-          </p> */}
-          <div className="mt-1 flex items-center gap-2">
-            <Pill>{tags?.[0]?.name || 'Sans tag'}</Pill>
+          <CustomInfoDialog title={title} description={description} />
+        </div>
 
-            <div className="text-muted-foreground flex items-center gap-x-2 text-xs">
-              <span>Complété par :</span>
-              <div className="origin-left scale-75">
-                <AvatarGroupDemo size="small" />
-              </div>
-              <span>4/10</span>
+        <div className="mt-1 flex items-center gap-2">
+          <Pill>{tags?.[0]?.name || 'Sans tag'}</Pill>
+
+          <div className="text-muted-foreground flex items-center gap-x-2 text-xs">
+            <span>Complété par :</span>
+            <div className="origin-left scale-75">
+              <AvatarGroupDemo size="small" />
             </div>
+            <span>4/10</span>
           </div>
         </div>
       </div>
