e5ba6b7792bf4bda7150c94276affc261f23c70f
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2025-06-12 11:47:58 +0200

    add dialog when clicking to validate action on challenge page

--- frontend/src/lib/graphql/generated/graphql-types.ts
+++ frontend/src/lib/graphql/generated/graphql-types.ts
@@ -323,11 +323,7 @@ export type UpdateChallengeMutationVariables = Exact<{
 
 export type UpdateChallengeMutation = {
   __typename?: 'Mutation';
-  updateChallenge: {
-    __typename?: 'Challenge';
-    id: string;
-    description?: string | null;
-  };
+  updateChallenge: { __typename?: 'Challenge'; id: string };
 };
 
 export type DeleteChallengeMutationVariables = Exact<{
@@ -929,7 +925,6 @@ export const UpdateChallengeDocument = gql`
   mutation UpdateChallenge($id: ID!, $data: ChallengeInput!) {
     updateChallenge(id: $id, data: $data) {
       id
-      description
     }
   }
 `;
