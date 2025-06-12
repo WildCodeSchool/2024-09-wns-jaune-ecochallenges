5e96a7e4b1b2dfcb4abdaad18ff2203a878124c3
Ornella B. <borges.ornella@gmail.com>
2025-06-12 12:43:59 +0200

    refactor(action-item): adjust styling and scale down AvatarGroupDemo for better layout integration

--- frontend/src/components/challenge-detail/ActionItem.tsx
+++ frontend/src/components/challenge-detail/ActionItem.tsx
@@ -1,6 +1,6 @@
-import { Checkbox } from "@/components/ui/checkbox";
-import { Pill } from "@/components";
-import  AvatarGroupDemo from "./avatar-group"; // Assure-toi que ce chemin est correct
+import { Checkbox } from '@/components/ui/checkbox';
+import { Pill } from '@/components';
+import AvatarGroupDemo from './avatar-group'; // Assure-toi que ce chemin est correct
 
 type Props = {
   id: string;
@@ -11,23 +11,32 @@ type Props = {
   onToggleStatus: (id: string) => void;
 };
 
-export const ActionItem = ({ id, title, description, tags, status, onToggleStatus }: Props) => {
+export const ActionItem = ({
+  id,
+  title,
+  description,
+  tags,
+  status,
+  onToggleStatus,
+}: Props) => {
   const isChecked = status === 'done';
 
   return (
-    <li className="flex items-center justify-between gap-2 p-4 rounded-xl shadow-sm">
+    <li className="flex items-center justify-between gap-2 rounded-xl bg-blue-300 p-4 shadow-sm">
       <div className="flex items-center gap-4">
         <div className="bg-zinc-200">
           <h3 className="block text-sm font-semibold">{title}</h3>
-          <p className="flex items-center gap-2 text-xs text-muted-foreground">
-          {description}
+          <p className="text-muted-foreground flex items-center gap-2 text-xs">
+            {description}
           </p>
           <div className="mt-1 flex items-center gap-2">
-            <Pill>{tags?.[0]?.name || "Sans tag"}</Pill>
+            <Pill>{tags?.[0]?.name || 'Sans tag'}</Pill>
 
-            <div className="flex items-center gap-2 text-xs text-muted-foreground">
-              <span>Complété par</span>
-              <AvatarGroupDemo />
+            <div className="text-muted-foreground flex items-center gap-x-2 text-xs">
+              <span>Complété par :</span>
+              <div className="origin-left scale-75">
+                <AvatarGroupDemo size="small" />
+              </div>
               <span>4/10</span>
             </div>
           </div>
