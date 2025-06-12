054297ea525c15b4df376166a344cafbfc1c9abd
Ornella B. <borges.ornella@gmail.com>
2025-06-12 10:42:45 +0200

    refactor(routes): change ChallengeDetailPage route path to /challenges/:challengeId

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -51,7 +51,7 @@ export const router = createBrowserRouter([
         element: <ChallengeEdition />,
       },
       {
-        path: '/challenge/:challengeId',
+        path: '/challenges/:challengeId',
         element: <ChallengeDetailPage />,
       },
       {
