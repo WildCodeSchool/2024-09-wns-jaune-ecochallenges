86ee6d810730401082e9b715749216ed5742afe5
Ornella B <borges.ornella@gmail.com>
2025-06-13 13:47:54 +0200

    fix: prevent crash by safely accessing user ID with optional chaining

--- frontend/src/components/challenge-detail/ChallengeDetail.tsx
+++ frontend/src/components/challenge-detail/ChallengeDetail.tsx
@@ -65,7 +65,7 @@ export const ChallengeDetail = ({ challengeId }: ChallengeDetailProps) => {
   const normalizeUAC = (uacs?: UserActionChallenge[]) =>
     uacs?.map((uac) => ({
       ...uac,
-      // comment: uac.comment ?? '',
+      comment: uac.comment ?? '',
       action: {
         ...uac.action,
         name: uac.action.name ?? '',
