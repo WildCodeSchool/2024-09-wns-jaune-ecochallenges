b7ed37a02d9cf30de940b84556cb42aa7ed1d216
marjorie <marjorie@homeexchange.com>
2025-07-08 16:40:15 +0200

    feat: add scoring to project

--- backend/src/entities/Challenge.ts
+++ backend/src/entities/Challenge.ts
@@ -14,7 +14,6 @@ import {
 import { Field, ID, ObjectType } from 'type-graphql';
 import { Action, User, UserActionChallengeScore } from '@/entities';
 import { Score } from './Score';
-import { ChallengeActionScore } from './ChallengeActionScore';
 
 @Entity()
 @ObjectType()
--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -12,7 +12,6 @@ import { Field, ObjectType } from 'type-graphql';
 import argon2 from 'argon2';
 import { Challenge, Action, UserActionChallengeScore } from '@/entities';
 import { Score } from './Score';
-import { ChallengeActionScore } from './ChallengeActionScore';
 
 export enum UserRole {
   ADMIN = 'admin',
@@ -69,13 +68,6 @@ export class User extends BaseEntity {
   @Field(() => Score)
   @OneToMany(() => Score, (score) => score.user)
   score?: Score;
-
-  @Field(() => [ChallengeActionScore])
-  @OneToMany(
-    () => ChallengeActionScore,
-    (challengeActionScore) => challengeActionScore.validatedBy
-  )
-  challengeActionScores?: ChallengeActionScore[];
 
   @Field()
   @Column({ nullable: true })
